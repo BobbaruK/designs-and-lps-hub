3d6ae2c0b6776ae8b7b2d9bd347fb9ce2de20cdd
Bobbaru <bobbaru_krueger@yahoo.com>
2025-01-14 00:55:09 +0200

    settings: page

--- webapp/src/app/(client)/settings/page.tsx
+++ webapp/src/app/(client)/settings/page.tsx
@@ -1,14 +1,29 @@
 "use client";
 
+import { PageBreadcrumbs } from "@/components/page-breadcrumbs";
+import { PageStructure } from "@/components/page-structure";
 import { PageTtle } from "@/components/page-title";
 import { SettingsForm } from "@/features/settings/components";
+import { IBreadcrumb } from "@/types";
+
+const BREADCRUMBS: IBreadcrumb[] = [
+  {
+    href: "/dashboard",
+    label: "Home",
+  },
+  {
+    href: "/settings",
+    label: "Settings",
+  },
+];
 
 const SettingsPage = () => {
   return (
-    <div className="container space-y-6">
+    <PageStructure>
+      <PageBreadcrumbs crumbs={BREADCRUMBS} />
       <PageTtle label="Settings" />
       <SettingsForm />
-    </div>
+    </PageStructure>
   );
 };
 
