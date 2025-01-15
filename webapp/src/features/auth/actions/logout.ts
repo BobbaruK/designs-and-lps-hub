397a249b1820ad971042e972d6b2162197bb4300
Bobbaru <bobbaru_krueger@yahoo.com>
2025-01-15 13:21:21 +0200

    auth: better logout

--- webapp/src/features/auth/actions/logout.ts
+++ webapp/src/features/auth/actions/logout.ts
@@ -2,13 +2,32 @@
 
 import { signOut } from "@/auth";
 
+// Centralize messages
+const MESSAGES = {
+  LOGGED_OUT_SUCCESS: "You have been signed out successfully!",
+  LOGGED_OUT_ERROR: "Something went wrong!",
+};
+
 /**
  * **{@linkcode logout} server function**
  *
  * A signout function. {@linkcode signOut}
  */
 export const logout = async () => {
-  await signOut({
-    redirectTo: "/auth/login",
-  });
+  try {
+    await signOut({
+      redirectTo: "/auth/login",
+      redirect: false,
+    });
+
+    return {
+      success: MESSAGES.LOGGED_OUT_SUCCESS,
+    };
+  } catch (error) {
+    console.error(MESSAGES.LOGGED_OUT_ERROR, error);
+
+    return {
+      error: MESSAGES.LOGGED_OUT_ERROR,
+    };
+  }
 };
--- webapp/src/features/auth/components/logout-button.tsx
+++ webapp/src/features/auth/components/logout-button.tsx
@@ -1,15 +1,35 @@
 "use client";
 
-import { signOut } from "next-auth/react";
 import { ReactNode } from "react";
+import { logout } from "../actions/logout";
+import { revalidate } from "@/actions/reavalidate";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 
 interface Props {
   children?: ReactNode;
 }
 
 export const LogoutButton = ({ children }: Props) => {
+  const router = useRouter();
+
+  const signOut = async () => {
+    logout().then((data) => {
+      if (data.error) {
+        toast.error(data.error);
+      }
+
+      if (data.success) {
+        toast.success(data.success);
+        router.push("/auth/login");
+      }
+
+      revalidate();
+    });
+  };
+
   return (
-    <span onClick={() => signOut()} className="cursor-pointer w-full">
+    <span onClick={signOut} className="w-full cursor-pointer">
       {children}
     </span>
   );
