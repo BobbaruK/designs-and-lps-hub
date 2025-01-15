b8b7f1b3f5194759cae1caf19fcdc27727c19256
Bobbaru <bobbaru_krueger@yahoo.com>
2025-01-15 13:21:09 +0200

    global: delete-dialog combo with drawer

--- webapp/src/components/delete-dialog.tsx
+++ webapp/src/components/delete-dialog.tsx
@@ -11,7 +11,18 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 import { MdDeleteOutline } from "react-icons/md";
+import { useMediaQuery } from "usehooks-ts";
 import { CustomButton } from "./custom-button";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "./ui/drawer";
 
 interface Props {
   label: string;
@@ -32,10 +43,54 @@ export const DeleteDialog = ({
   showTrigger = true,
   hideLabelOnMobile = true,
 }: Props) => {
+  const isDesktop = useMediaQuery("(min-width: 768px)");
+
+  if (isDesktop) {
+    return (
+      <Dialog open={open} onOpenChange={onOpenChange}>
+        {showTrigger && (
+          <DialogTrigger asChild>
+            <CustomButton
+              buttonLabel={`Delete ${asset}`}
+              variant={"destructive"}
+              icon={MdDeleteOutline}
+              iconPlacement="left"
+              hideLabelOnMobile={hideLabelOnMobile}
+            />
+          </DialogTrigger>
+        )}
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Are you absolutely sure?</DialogTitle>
+            <DialogDescription>
+              This will permanently delete {asset} <strong>{label}</strong> and
+              remove all data from the servers. This action cannot be undone.
+            </DialogDescription>
+          </DialogHeader>
+          <DialogFooter className="gap-2 sm:justify-start">
+            <DialogClose asChild>
+              <CustomButton buttonLabel={`Cancel`} variant={"secondary"} />
+            </DialogClose>
+            <DialogClose asChild>
+              <CustomButton
+                buttonLabel={`Delete ${asset}`}
+                variant={"destructive"}
+                icon={MdDeleteOutline}
+                iconPlacement="left"
+                onClick={onDelete}
+                hideLabelOnMobile={false}
+              />
+            </DialogClose>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    );
+  }
+
   return (
-    <Dialog open={open} onOpenChange={onOpenChange}>
+    <Drawer open={open} onOpenChange={onOpenChange}>
       {showTrigger && (
-        <DialogTrigger asChild>
+        <DrawerTrigger asChild>
           <CustomButton
             buttonLabel={`Delete ${asset}`}
             variant={"destructive"}
@@ -43,21 +98,21 @@ export const DeleteDialog = ({
             iconPlacement="left"
             hideLabelOnMobile={hideLabelOnMobile}
           />
-        </DialogTrigger>
+        </DrawerTrigger>
       )}
-      <DialogContent>
-        <DialogHeader>
-          <DialogTitle>Are you absolutely sure?</DialogTitle>
-          <DialogDescription>
+      <DrawerContent>
+        <DrawerHeader className="text-left">
+          <DrawerTitle>Are you absolutely sure?</DrawerTitle>
+          <DrawerDescription>
             This will permanently delete {asset} <strong>{label}</strong> and
             remove all data from the servers. This action cannot be undone.
-          </DialogDescription>
-        </DialogHeader>
-        <DialogFooter className="sm:justify-start gap-2">
-          <DialogClose asChild>
+          </DrawerDescription>
+        </DrawerHeader>
+        <DrawerFooter className="pt-2 flex flex-col sm:flex-row">
+          <DrawerClose asChild>
             <CustomButton buttonLabel={`Cancel`} variant={"secondary"} />
-          </DialogClose>
-          <DialogClose asChild>
+          </DrawerClose>
+          <DrawerClose asChild>
             <CustomButton
               buttonLabel={`Delete ${asset}`}
               variant={"destructive"}
@@ -66,9 +121,9 @@ export const DeleteDialog = ({
               onClick={onDelete}
               hideLabelOnMobile={false}
             />
-          </DialogClose>
-        </DialogFooter>
-      </DialogContent>
-    </Dialog>
+          </DrawerClose>
+        </DrawerFooter>
+      </DrawerContent>
+    </Drawer>
   );
 };
