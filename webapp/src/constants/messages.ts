beb348ba6a34111b88404da33aa595f14d45acab
Bobbaru <bobbaru_krueger@yahoo.com>
2025-01-17 19:31:17 +0200

    globals: messages update

--- webapp/src/constants/messages.ts
+++ webapp/src/constants/messages.ts
@@ -1,18 +1,39 @@
 export const ACTION_MESSAGES = (resource?: string) => {
   return {
+    SUCCESS_ADD: resource
+      ? `${resource} sucessfuly added!`
+      : "Sucessfuly added!",
+    SUCCESS_UPDATE: resource
+      ? `${resource} sucessfuly updated!`
+      : "Sucessfuly updated!",
+    SUCCESS_DELETE: resource
+      ? `${resource} sucessfuly deleted!`
+      : "Sucessfuly deleted!",
     COULD_NOT_ADD: resource
       ? `${resource} could not be added!`
       : "Could not be added!",
-    SUCCESS_ADD: resource
-      ? `${resource} sucessfuly added2!`
-      : "Sucessfuly added!",
+    COULD_NOT_UPDATE: resource
+      ? `${resource} could not be updated!`
+      : "Could not be updated!",
+    COULD_NOT_DELETE: resource
+      ? `${resource} could not be deleted!`
+      : "Could not be deleted!",
     WENT_WRONG: "Something went wrong!",
     EMAIL_IN_USE: "Email already in use",
+    UNAUTHORIZED: "Unauthorized!",
+    INVALID_FIELDS: "Invalid fields!",
+    DOES_NOT_EXISTS: resource
+      ? `${resource} does not exists!`
+      : "Does not exists!",
+    VERIFICATION_SENT: "Verification email sent!",
+    NOT_OLD_PASS: "User's new password cannot be old password!",
   };
 };
 
 export const PRISMA_MESSAGES = (resource?: string) => {
   return {
-    P2002: resource ? `Duplicate ${resource.toLowerCase()}!` : "Duplicate!",
+    P2002: resource
+      ? `Already exists a resource with this ${resource.toLowerCase()}!`
+      : "Duplicate!",
   };
 };
