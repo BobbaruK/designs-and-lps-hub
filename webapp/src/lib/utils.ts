20dd8fee74bdb2c670afa64db89c88d2284b1529
Bobbaru <bobbaru_krueger@yahoo.com>
2025-03-29 02:35:09 +0200

    globals: capitalize 1st letter util fn

--- webapp/src/lib/utils.ts
+++ webapp/src/lib/utils.ts
@@ -78,3 +78,8 @@ export const prismaError = (
       return { error: ACTION_MESSAGES().COULD_NOT_ADD };
   }
 };
+
+export const capitalizeFirstLetter = (str: string): string => {
+  if (!str) return str;
+  return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+};
