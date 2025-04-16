e0c455f7e365f7c08037c8e91070c04148222d69
Bobbaru <bobbaru_krueger@yahoo.com>
2025-04-16 16:57:04 +0300

    shared: custom avatar compo update

--- webapp/src/components/custom-avatar.tsx
+++ webapp/src/components/custom-avatar.tsx
@@ -4,17 +4,25 @@ import { FaUser } from "react-icons/fa";
 
 interface Props extends React.HTMLAttributes<HTMLSpanElement> {
   image?: string | null;
+  fit?: "cover" | "contain";
+  icon?: React.ReactNode;
 }
 
-export const CustomAvatar = ({ image, ...restProps }: Props) => {
+export const CustomAvatar = ({ image, fit, icon, ...restProps }: Props) => {
   return (
     <Avatar
       {...restProps}
       className={cn("max-w-full border border-primary", restProps.className)}
     >
-      <AvatarImage src={image || undefined} className={"object-cover"} />
+      <AvatarImage
+        src={image || undefined}
+        className={cn(
+          ``,
+          fit === "cover" || !fit ? "object-cover" : "object-contain",
+        )}
+      />
       <AvatarFallback className="rounded-none">
-        <FaUser className="h-[55%] w-[55%]" />
+        {icon || <FaUser className="h-[55%] w-[55%]" />}
       </AvatarFallback>
     </Avatar>
   );
