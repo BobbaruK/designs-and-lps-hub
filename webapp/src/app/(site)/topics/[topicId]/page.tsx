fe93c2016f7c6911b79278b1fec31261237c5946
Bobbaru <bobbaru_krueger@yahoo.com>
2025-04-25 01:51:14 +0300

    app: topics landing pages table update

--- webapp/src/app/(site)/topics/[topicId]/page.tsx
+++ webapp/src/app/(site)/topics/[topicId]/page.tsx
@@ -1,29 +1,123 @@
-import { DataTable } from "@/components/data-table";
 import { PageBreadcrumbs } from "@/components/page-breadcrumbs";
 import { PageStructure } from "@/components/page-structure";
 import { PageTitle } from "@/components/page-title";
+import { loadSearchParams } from "@/components/search-params";
+import { DataTableTransitionWrapper } from "@/components/table/landing-pages/data-table-transition-wrapper";
 import { topicsMeta } from "@/constants/page-titles/topics";
-import { LandingPageLegend } from "@/features/landing-pages/components/landing-page-legend";
-import { columns } from "@/features/landing-pages/components/table/landing-page-columns";
+import { getBrandsMinimal } from "@/features/brands/data/get-brands";
+import { getLandingPageFeaturesMinimal } from "@/features/landing-page-features/data/get-landing-page-features";
+import { getLandingPageTypesMinimal } from "@/features/landing-page-types/data/get-landing-page-types";
+import { getLandingPagesFilteredCount } from "@/features/landing-pages/data/get-landing-pages";
+import { getLanguagesMinimal } from "@/features/languages/data/get-languages";
+import { getLicensesMinimal } from "@/features/licenses/data/get-licenses";
+import { getRegistrationTypesMinimal } from "@/features/registration-types/data/get-registration-types";
 import { getTopicBySlug } from "@/features/topics/data/get-topic";
+import { getTopicsMinimal } from "@/features/topics/data/get-topics";
 import { breadCrumbsFn } from "@/lib/breadcrumbs";
+import { lpsWhere } from "@/lib/filtering";
+import { lpsOrderBy } from "@/lib/sorting";
 import { capitalizeFirstLetter } from "@/lib/utils";
 import { notFound } from "next/navigation";
+import { SearchParams } from "nuqs/server";
 
 interface Props {
   params: Promise<{
     topicId: string;
   }>;
+  searchParams: Promise<SearchParams>;
 }
 
-const TopicPage = async ({ params }: Props) => {
+const TopicPage = async ({ params, searchParams }: Props) => {
   const { topicId } = await params;
+
+  const {
+    // Filters
+    feature,
+    brand,
+    registrationType,
+    language,
+    topic,
+    license,
+    lpType,
+    isARTS,
+    isReadyForTraffic,
+    whatsapp,
+    operator,
+    // Pagination
+    pageIndex,
+    pageSize,
+    // Sorting
+    sortBy,
+    sort,
+    // Search
+    search,
+  } = await loadSearchParams(searchParams);
+
+  const lpsFilters = lpsWhere({
+    filters: {
+      feature,
+      brand,
+      topic,
+      registrationType,
+      language,
+      license,
+      landingPageType: lpType,
+      isARTS,
+      isReadyForTraffic,
+      whatsapp,
+      operator,
+      search,
+    },
+  });
+
+  const orderBy = lpsOrderBy({ sort, sortBy });
+
+  const features = await getLandingPageFeaturesMinimal();
+
+  const topics = await getTopicsMinimal();
+
+  const licenses = await getLicensesMinimal();
+
+  const landingPageTypes = await getLandingPageTypesMinimal();
+
+  const registrationTypes = await getRegistrationTypesMinimal();
+
+  const languages = await getLanguagesMinimal();
+
+  const brands = await getBrandsMinimal();
+
+  const showResetAll =
+    (feature && feature.length > 0) ||
+    (topic && topic.length > 0) ||
+    (brand && brand.length > 0) ||
+    (registrationType && registrationType.length > 0) ||
+    (language && language.length > 0) ||
+    (license && license.length > 0) ||
+    (lpType && lpType.length > 0) ||
+    typeof isARTS === "boolean" ||
+    typeof isReadyForTraffic === "boolean" ||
+    typeof whatsapp === "boolean" ||
+    operator !== null
+      ? true
+      : false;
 
   const topicHref = `${topicsMeta.href}/${topicId}`;
 
-  const topic = await getTopicBySlug(topicId);
+  const actualTopic = await getTopicBySlug({
+    slug: topicId,
+    lpsWhere: lpsFilters,
+    orderBy,
+    pageNumber: pageIndex,
+    perPage: pageSize,
+  });
+  const landingPagesCount = await getLandingPagesFilteredCount({
+    topic: {
+      slug: topicId,
+    },
+    ...lpsFilters,
+  });
 
-  if (!topic) notFound();
+  if (!actualTopic) notFound();
 
   return (
     <PageStructure>
@@ -35,18 +129,18 @@ const TopicPage = async ({ params }: Props) => {
           },
           {
             href: topicHref,
-            label: topic.name,
+            label: actualTopic.name,
           },
         ])}
       />
       <PageTitle
-        label={topic.name}
+        label={actualTopic.name}
         backBtnHref={topicsMeta.href}
         editBtnHref={`${topicHref}/edit`}
       />
       <div>
-        {topic.description ? (
-          <pre className="whitespace-pre-wrap">{topic.description}</pre>
+        {actualTopic.description ? (
+          <pre className="whitespace-pre-wrap">{actualTopic.description}</pre>
         ) : (
           <p>
             <span className="italic">There is no description added</span>
@@ -56,20 +150,20 @@ const TopicPage = async ({ params }: Props) => {
 
       <section>
         <h2 className="text-heading4">Landing pages</h2>
-        <DataTable
-          columns={columns}
-          data={topic.landingPages}
-          columnVisibilityObj={{
-            slug: false,
-            fxoroFooter: false,
-            requester: false,
-            topic: false,
-            createdAt: false,
-            createdBy: false,
-            updatedAt: false,
-            updatedBy: false,
+
+        <DataTableTransitionWrapper
+          data={actualTopic.landingPages}
+          filters={{
+            features: features,
+            topics: topics,
+            licenses: licenses,
+            landingPageTypes: landingPageTypes,
+            registrationTypes: registrationTypes,
+            languages: languages,
+            brands: brands,
+            showResetAll: showResetAll,
           }}
-          legendItems={<LandingPageLegend />}
+          dataCount={landingPagesCount}
         />
       </section>
     </PageStructure>
