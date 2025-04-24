20db2b1aff9794c1c6f834736802d7c88d7921b9
Bobbaru <bobbaru_krueger@yahoo.com>
2025-04-25 01:57:16 +0300

    app: licenses landing pages table update

--- webapp/src/features/licenses/data/get-license.ts
+++ webapp/src/features/licenses/data/get-license.ts
@@ -1,4 +1,6 @@
+import { PAGINATION_DEFAULT } from "@/constants/table";
 import db from "@/lib/db";
+import { Prisma } from "@prisma/client";
 
 /**
  * {@linkcode getLicenseBySlug}
@@ -6,7 +8,22 @@ import db from "@/lib/db";
  * @param {string} slug - search in the database by id
  * @yields a `Promise` that resolve in an user `Object`
  */
-export const getLicenseBySlug = async (slug: string) => {
+export const getLicenseBySlug = async ({
+  slug,
+  lpsWhere,
+  orderBy,
+  pageNumber,
+  perPage,
+}: {
+  slug: string;
+  lpsWhere?: Prisma.dl_landing_pageWhereInput;
+  orderBy?: Prisma.dl_landing_pageOrderByWithRelationInput;
+  perPage?: number | null;
+  pageNumber?: number | null;
+}) => {
+  const pageSize = perPage || PAGINATION_DEFAULT;
+  const skip = pageNumber ? pageNumber * pageSize : 0;
+
   try {
     const license = await db.dl_license.findUnique({
       where: {
@@ -49,9 +66,10 @@ export const getLicenseBySlug = async (slug: string) => {
             topic: true,
             features: true,
           },
-          orderBy: {
-            createdAt: "desc",
-          },
+          skip,
+          take: pageSize,
+          ...(orderBy ? { orderBy } : {}),
+          ...(lpsWhere ? { where: lpsWhere } : {}),
         },
       },
     });
