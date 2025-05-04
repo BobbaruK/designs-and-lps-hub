092f3548f346b727fdadf04c4120d7eccdddc508
Bobbaru <bobbaru_krueger@yahoo.com>
2025-05-05 02:46:32 +0300

    shared: copy-to-clipboard refactoring

--- webapp/src/components/copy-to-clipboard.tsx
+++ webapp/src/components/copy-to-clipboard.tsx
@@ -1,47 +1,65 @@
 "use client";
 
-import { BUTTON_EFFECT } from "@/constants/button";
 import { ACTION_MESSAGES } from "@/constants/messages";
 import { FaRegCopy } from "react-icons/fa";
 import { toast } from "sonner";
-import { Button, ButtonProps } from "./ui/button";
+import { useCopyToClipboard } from "usehooks-ts";
+import { CustomButton } from "./custom-button";
+import { ButtonProps } from "./ui/button";
 
 interface Props extends ButtonProps {
   data: string;
 }
 
-export const CopyToClipboard = ({ data, ...props }: Props) => {
-  const copyAndToast = async () => {
-    try {
-      await navigator.clipboard.writeText(data);
+export const CopyToClipboard = ({ data, ...restProps }: Props) => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [copiedText, copy] = useCopyToClipboard();
 
-      toast.info(
-        <div className="space-y-2">
-          <p className="font-bold">Copied to clipboard.</p>
-          <p className="line-clamp-2">{data}</p>
-        </div>,
-      );
-    } catch {
-      toast.error(
-        <div className="space-y-2">
-          <p className="font-bold">{ACTION_MESSAGES().WENT_WRONG}</p>
-          <p className="line-clamp-2">Failed to copy to clipboard</p>
-        </div>,
-      );
-    }
+  const handleCopy = (text: string) => () => {
+    copy(text)
+      .then(() => {
+        toast.info(<ToastBody type={"success"} copiedData={data} />);
+      })
+      .catch((error) => {
+        console.error("Failed to copy!", error);
+        toast.error(<ToastBody type={"error"} />);
+      });
   };
 
   return (
-    <>
-      <Button
-        variant={"info"}
-        onClick={copyAndToast}
-        className="flex size-7 p-1 min-w-7"
-        effect={BUTTON_EFFECT}
-        {...props}
-      >
-        <FaRegCopy />
-      </Button>
-    </>
+    <CustomButton
+      buttonLabel={`Copy to clipboard`}
+      variant={"info"}
+      size={"icon"}
+      onClick={handleCopy(data)}
+      icon={FaRegCopy}
+      iconPlacement="right"
+      {...restProps}
+    />
   );
 };
+
+export function ToastBody({
+  type,
+  copiedData,
+}: {
+  type: "error" | "success";
+  copiedData?: string;
+}) {
+  return (
+    <div className="space-y-2">
+      {type === "success" && (
+        <>
+          <p className="font-bold">Copied to clipboard.</p>
+          <p className="line-clamp-2">{copiedData}</p>
+        </>
+      )}
+      {type === "error" && (
+        <>
+          <p className="font-bold">{ACTION_MESSAGES().WENT_WRONG}</p>
+          <p className="line-clamp-2">Failed to copy to clipboard</p>
+        </>
+      )}
+    </div>
+  );
+}
