00abc53fa71dc7577b01d14fca1b949d430c7b7d
Bobbaru <bobbaru_krueger@yahoo.com>
2025-05-04 18:14:29 +0300

    topics: solve a small bug in row action

--- webapp/src/features/topics/components/table/topic-row-actions.tsx
+++ webapp/src/features/topics/components/table/topic-row-actions.tsx
@@ -10,6 +10,7 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { ACTION_MESSAGES } from "@/constants/messages";
+import { topicsMeta } from "@/constants/page-titles/topics";
 import { useCurrentRole } from "@/features/auth/hooks/use-current-role";
 import { dl_topic, UserRole } from "@prisma/client";
 import { MoreHorizontal } from "lucide-react";
@@ -17,7 +18,6 @@ import Link from "next/link";
 import { useState } from "react";
 import { toast } from "sonner";
 import { deleteTopic } from "../../actions/delete-topic";
-import { topicsMeta } from "@/constants/page-titles/topics";
 
 interface Props {
   topic: dl_topic;
@@ -25,6 +25,7 @@ interface Props {
 
 const TopicRowActions = ({ topic }: Props) => {
   const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const userRole = useCurrentRole();
 
   const onDelete = () => {
@@ -53,7 +54,7 @@ const TopicRowActions = ({ topic }: Props) => {
         showTrigger={false}
       />
 
-      <DropdownMenu>
+      <DropdownMenu open={isDropdownOpen} onOpenChange={setIsDropdownOpen}>
         <DropdownMenuTrigger asChild>
           <Button variant="ghost" className="h-8 w-8 p-0">
             <span className="sr-only">Open menu</span>
@@ -73,8 +74,11 @@ const TopicRowActions = ({ topic }: Props) => {
                 </Link>
               </DropdownMenuItem>
               <DropdownMenuItem
-                onClick={() => {
+                onClick={(evt) => {
+                  evt.preventDefault();
+
                   setIsDialogOpen(true);
+                  setIsDropdownOpen(false);
                 }}
               >
                 <span>
