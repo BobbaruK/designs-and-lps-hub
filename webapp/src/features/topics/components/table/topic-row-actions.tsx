049f3ae04ff83b059b444d7419d7677a587b8cb3
Bobbaru <bobbaru_krueger@yahoo.com>
2025-05-05 16:16:12 +0300

    topics: row actions copy update

--- webapp/src/features/topics/components/table/topic-row-actions.tsx
+++ webapp/src/features/topics/components/table/topic-row-actions.tsx
@@ -1,4 +1,5 @@
 import { revalidate } from "@/actions/reavalidate";
+import { ToastBody } from "@/components/copy-to-clipboard/toast-body";
 import { DeleteDialog } from "@/components/delete-dialog";
 import { Button } from "@/components/ui/button";
 import {
@@ -12,6 +13,7 @@ import {
 import { ACTION_MESSAGES } from "@/constants/messages";
 import { topicsMeta } from "@/constants/page-titles/topics";
 import { useCurrentRole } from "@/features/auth/hooks/use-current-role";
+import { useCustomCopy } from "@/hooks/use-custom-copy";
 import { dl_topic, UserRole } from "@prisma/client";
 import { MoreHorizontal } from "lucide-react";
 import Link from "next/link";
@@ -27,6 +29,7 @@ const TopicRowActions = ({ topic }: Props) => {
   const [isDialogOpen, setIsDialogOpen] = useState(false);
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const userRole = useCurrentRole();
+  const { handleCopy } = useCustomCopy();
 
   const onDelete = () => {
     // TODO: use transition here and other similar places
@@ -90,13 +93,13 @@ const TopicRowActions = ({ topic }: Props) => {
           )}
           <DropdownMenuSeparator />
           <DropdownMenuItem
-            onClick={() => {
-              navigator.clipboard.writeText(topic.id);
-
-              toast.info(`Copied ${topic.name}'s ID`, {
-                description: topic.id,
-              });
-            }}
+            onClick={handleCopy({
+              text: topic.id,
+              toastError: <ToastBody type={"error"} />,
+              toastSuccess: (
+                <ToastBody type={"success"} copiedData={topic.id} />
+              ),
+            })}
           >
             <span>
               Copy <strong>{topic.name}</strong>&apos;s ID
