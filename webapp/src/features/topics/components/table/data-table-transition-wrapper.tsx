6bf8fd6e2dd8efe49a2cdccb6d05af9778aceccb
Bobbaru <bobbaru_krueger@yahoo.com>
2025-05-20 01:56:25 +0300

    topics: data-table-transition-wrapper update

--- webapp/src/features/topics/components/table/data-table-transition-wrapper.tsx
+++ webapp/src/features/topics/components/table/data-table-transition-wrapper.tsx
@@ -9,6 +9,7 @@ import { useTransition } from "react";
 import { toast } from "sonner";
 import { deleteManyTopics } from "../../actions/delete-topic";
 import { columns } from "./columns";
+import TableProvider from "@/components/data-table-server-rendered/table-provider";
 
 interface Props {
   data: DB_Topic[];
@@ -43,28 +44,23 @@ export const DataTableTransitionWrapper = ({
   };
 
   return (
-    <>
+    <TableProvider
+      handleDelete={handleDelete}
+      isLoading={isLoading}
+      startTransition={startTransition}
+      dataSelected={{
+        type: "topics",
+        data: dataSelected || null,
+      }}
+      dataCount={dataCount || 0}
+    >
       <DataTable
         columns={columns({ isLoading, startTransition, visibleTopics: data })}
         data={data}
-        dataSelected={{
-          type: "topics",
-          data: dataSelected || null,
-        }}
-        dataCount={dataCount}
         columnVisibilityObj={columnVisibilityObj}
-        advancedFiltering={
-          <LandingPageFiltering
-            showResetAll={showResetAll}
-            isLoading={isLoading}
-            startTransition={startTransition}
-          />
-        }
+        advancedFiltering={<LandingPageFiltering showResetAll={showResetAll} />}
         twSkeletonHeightCell="h-[51px]"
-        isLoading={isLoading}
-        startTransition={startTransition}
-        handleDelete={handleDelete}
       />
-    </>
+    </TableProvider>
   );
 };
